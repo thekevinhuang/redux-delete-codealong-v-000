298e6c8ad3e5172be8d7dd11f5f613c765e42193
Kevin Huang <yellow.khaos@gmail.com>
2019-02-11 04:40:46 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -13,7 +13,9 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
-    this.setState({text: ''})
+    this.setState({
+      text: ''
+    })
   }
 
   handleChange(event) {
